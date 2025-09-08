50c4afd9a54080b7dcb495cbb08115bb5376f3de
lihanrui2913 <lihanrui20091103@126.com>
2025-09-09 06:57:55 +0800

    注册socketv6

--- kernel/modules/netserver/netserver.c
+++ kernel/modules/netserver/netserver.c
@@ -1,10 +1,12 @@
 #include "netserver.h"
 
 extern void real_socket_init();
+extern void real_socket_v6_init();
 
 __attribute__((visibility("default"))) int dlmain()
 {
     real_socket_init();
+    real_socket_v6_init();
 
     return 0;
 }
